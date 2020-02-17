6a3c8094a1cf83686546ba86082342d7348c67c3
Grzegorz Baczek <gb@cardlay.com>
2020-02-17 23:13:32 +0100

    edit

--- Activities_exercise_1/app/src/main/java/com/programming/adnroid/sdu/activities_exercise_1/Constants.java
+++ Activities_exercise_1/app/src/main/java/com/programming/adnroid/sdu/activities_exercise_1/Constants.java
@@ -5,8 +5,10 @@ package com.programming.adnroid.sdu.activities_exercise_1;
  */
 
 public class Constants {
+
     public static final String TAG = "activities_events";
     public static final String NAME_KEY = "NAME_KEY";
     public static final String ADDRESS_KEY = "ADDRESS_KEY";
     public static final String DATE_OF_BIRTH_KEY = "DATE_OF_BIRTH_KEY";
+
 }
