ccf27d94e75726559fb10ff95fe315082031a585
Grzegorz Baczek <gb@cardlay.com>
2020-03-22 07:52:12 +0100

    migrating to androidx

--- ContentProviderExercise2/app/src/main/java/com/programming/android/sdu/contentproviderexercise/MainActivity.java
+++ ContentProviderExercise2/app/src/main/java/com/programming/android/sdu/contentproviderexercise/MainActivity.java
@@ -10,9 +10,9 @@ import android.os.Build;
 import android.provider.ContactsContract;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
