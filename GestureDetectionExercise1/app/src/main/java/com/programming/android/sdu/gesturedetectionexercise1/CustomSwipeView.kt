6368241c84b52b6d5057fe355859e9d146cd4803
em135 <emilqz@gmail.com>
2021-02-09 23:11:04 +0100

    Refactor to Androidx

--- GestureDetectionExercise1/app/src/main/java/com/programming/android/sdu/gesturedetectionexercise1/CustomSwipeView.kt
+++ GestureDetectionExercise1/app/src/main/java/com/programming/android/sdu/gesturedetectionexercise1/CustomSwipeView.kt
@@ -1,6 +1,5 @@
 package com.programming.android.sdu.gesturedetectionexercise1
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
--- GestureDetectionExercise1/app/src/main/java/com/programming/android/sdu/gesturedetectionexercise1/MainActivity.kt
+++ GestureDetectionExercise1/app/src/main/java/com/programming/android/sdu/gesturedetectionexercise1/MainActivity.kt
@@ -1,8 +1,7 @@
 package com.programming.android.sdu.gesturedetectionexercise1
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-
+import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
